b149999418ef0d845f5cd87b471af660e682ab6a
l3miage-campusb <bastien.campus@etu.univ-grenoble-alpes.fr>
2024-02-16 09:42:44 +0100

    premier test composant matrix

---
+++ src/app/matrix/matrix.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Matrix } from '../matrix';
+
+@Component({
+  selector: 'app-matrix',
+  templateUrl: './matrix.component.html',
+  styleUrls: ['./matrix.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MatrixComponent {
+
+  @Input({ required: true }) data!: Matrix<number,number,number>
+
+    
+}
+
