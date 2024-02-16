2dd1e983a007906c612f08e74e80713f82f92680
l3miage-campusb <bastien.campus@etu.univ-grenoble-alpes.fr>
2024-02-16 10:35:39 +0100

    debug html et3

--- src/app/matrix/matrix.component.ts
+++ src/app/matrix/matrix.component.ts
@@ -9,7 +9,7 @@ import { Matrix } from '../matrix';
 })
 export class MatrixComponent {
 
-  @Input({ required: true }) data!: Matrix<number,number,number>   
+  @Input({ required: true }) data!: Matrix<number,number,number>  | undefined 
 
 
 }
